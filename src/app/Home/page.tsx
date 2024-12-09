139879528df54cce2c692803e826255d9871d23a
M .Ayyaz <160455218+Dogoodhavegood@users.noreply.github.com>
2024-12-09 17:06:50 +0500

    Add files via upload

---
+++ src/app/Home/page.tsx
@@ -0,0 +1,20 @@
+
+export default function Hero (){
+    return(
+        <div
+        className="flex md:justify-end justify-center items-center w-[1440px] h-[316px] "
+
+        // className="flex md:justify-end justify-center items-center  md: w-[1440px] h-[316px] bg-no-repeat bg-center bg-contain bg-[#F4F5F7]" 
+        style={{
+            backgroundImage: `url("/images/homehero.jpg")`,
+          
+        }}>
+            
+            
+
+    
+            
+        </div>
+        
+    )
+}
